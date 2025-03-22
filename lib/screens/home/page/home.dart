5068041d9ee968fb2b51fca66197f4ceb47cd3b8
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-03-22 10:39:09 +0600

    seperate_navigation_bar

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -1,6 +1,8 @@
+import 'package:ecommerce_nahid/screens/home/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import '../../../common/widgets/appbar.dart';
+// Import the new BottomNavBar widget
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,7 +15,64 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    Container(color: const Color.fromARGB(255, 224, 19, 19)),
+    Column(
+      children: [
+        Center(
+          child: Card(
+            margin: const EdgeInsets.all(8.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            elevation: 4,
+            child: SizedBox(
+              width: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset('assets/images/image.png', width: 150),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "TMA-2 HD Wireless",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "Rp. 1.500.000",
+                      style: TextStyle(fontSize: 14, color: Colors.red),
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.star, color: Colors.amber, size: 14),
+                        SizedBox(width: 4),
+                        Text(
+                          "4.6",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                        SizedBox(width: 6),
+                        Text(
+                          "86 Reviews",
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
     Container(color: Colors.blue),
     Container(color: Colors.green),
     Container(color: Colors.orange),
@@ -30,28 +89,9 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: FAppBar(title: "Mega Shop", backButton: true),
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
+      bottomNavigationBar: FBottomBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.home_outline),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.heart_outline),
-            label: "Wishlist",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.shopping_bag_outline),
-            label: "Order",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.user_add_bold),
-            label: "Accounts",
-          ),
-        ],
       ),
     );
   }
