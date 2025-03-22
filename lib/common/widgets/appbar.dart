3322ee261b1542ba8739aff67711a2b126f78807
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-03-22 10:02:09 +0600

    Add_Bottom_Navigation

---
+++ lib/common/widgets/appbar.dart
@@ -0,0 +1,46 @@
+import 'package:ecommerce_nahid/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({
+    super.key,
+    required this.title,
+    this.firstIcon = Iconsax.notification_bing_outline,
+    this.secondIcon = Icons.shopping_cart,
+    this.backButton = false,
+  });
+
+  final String title;
+  final IconData firstIcon;
+  final IconData secondIcon;
+  final bool backButton;
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: backButton ? Icon(Iconsax.arrow_left_2_outline) : null,
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.bold,
+          color: FColors.oceanBlue,
+        ),
+      ),
+      centerTitle: true,
+      actions: [
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(firstIcon, color: FColors.dark),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+          child: Icon(secondIcon, color: FColors.dark),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/home/page/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import '../../../common/widgets/appbar.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Container(color: Colors.white),
+    Container(color: Colors.blue),
+    Container(color: Colors.green),
+    Container(color: Colors.orange),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FAppBar(title: "Mega Shop", backButton: true),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.home_outline),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.heart_outline),
+            label: "Wishlist",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.shopping_bag_outline),
+            label: "Order",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.user_add_bold),
+            label: "Accounts",
+          ),
+        ],
+      ),
+    );
+  }
+}
